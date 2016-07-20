4f431234398da30f21910c8ce7eb2655f79db4d1
cardidemonaco-work <cdemonaco@courts.mi.gov>
2016-07-20 15:08:29 -0400

    Continued with project

---
+++ bookkeeping/bookkeeping/Common/Transaction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using bookkeeping.App_Data;
+
+namespace bookkeeping
+{
+    public class Transaction
+    {
+        public Transaction()
+        {
+            //asdf
+        }
+
+        DataClassesDataContext _dc = new DataClassesDataContext();
+
+        /// <summary>
+        /// Inserts a Transaction (Revenue or Expense) into the database
+        /// </summary>
+        /// <param name="name">Name of the Transaction (Revenue or Expense)</param>
+        /// <param name="description">Description of the Transaction (Revenue or Expense)</param>
+        /// <param name="amount">Amount of the Transaction (Revenue or Expense)</param>
+        public void Insert(string name, string description, decimal amount)
+        {
+            //Put together LINQ statement and INSERT into the database
+            transaction rev = new transaction
+            {
+                transaction_name = name,
+                transaction_description = description,
+                transaction_amount = amount
+            };
+
+            _dc.transactions.InsertOnSubmit(rev);
+            _dc.SubmitChanges();
+        }
+    }
+}
--- bookkeeping/bookkeeping/Revenue.aspx.cs
+++ bookkeeping/bookkeeping/Revenue.aspx.cs
@@ -1,21 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using bookkeeping.App_Data;
-
+using bookkeeping;
 
 namespace bookkeeping
 {
-
-
     public partial class Revenue : System.Web.UI.Page
     {
-        DataClassesDataContext _dc = new DataClassesDataContext();
+        //DataClassesDataContext _dc = new DataClassesDataContext();
 
-        //protected void Page_Load(object sender, EventArgs e)
-        //{
-
-        //}
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string test;
+            test = "test";
+            test = "new test";
+        }
 
         protected void btnRevenueSubmit_Click(object sender, EventArgs e)
         {
@@ -25,27 +24,8 @@ namespace bookkeeping
             decimal amount = Convert.ToDecimal(txtAmount.Text);
 
             //Insert Transaction into the database
-            TransactionInsert(name, desc, amount);
-        }
-
-        /// <summary>
-        /// Inserts a Transaction (Revenue or Expense) into the database
-        /// </summary>
-        /// <param name="name">Name of the Transaction (Revenue or Expense)</param>
-        /// <param name="description">Description of the Transaction (Revenue or Expense)</param>
-        /// <param name="amount">Amount of the Transaction (Revenue or Expense)</param>
-        public void TransactionInsert(string name, string description, decimal amount)
-        {
-            //Put together LINQ statement and INSERT into the database
-            transaction rev = new transaction
-            {
-                transaction_name = name,
-                transaction_description = description,
-                transaction_amount = amount
-            };
-
-            _dc.transactions.InsertOnSubmit(rev);
-            _dc.SubmitChanges();
+            Transaction t = new Transaction();
+            t.Insert(name, desc, amount);
         }
     }
 }
